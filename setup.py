e9abc54d592be72503eb76440c2b6d64106d676b
Kantesh Raj <kanteshraj@gmail.com>
2015-09-04 13:38:33 +0530

    setup.py added for pip install

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup
+
+
+if __name__ == '__main__' and sys.version_info < (2, 5):
+    raise SystemExit("Python >= 2.5 required for virtualenv-clone")
+
+
+setup(name="virtualenv-relocate",
+    version='0.1.0',
+    description='script to relocate virtualenvs',
+    author='Kantesh Raj',
+    author_email='kanteshraj@gmail.com',
+    url='https://github.com/kanteshraj/virtualenv-relocate',
+    license="MIT",
+    py_modules=["virtualenv-relocate"],
+    entry_points={
+        'console_scripts': [
+            'virtualenv-relocate=virtualenv-relocate:main',
+    ]},
+    classifiers=[
+        "License :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+    ]
+)
